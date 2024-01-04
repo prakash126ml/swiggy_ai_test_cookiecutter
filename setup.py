fbb8dd409f988c9cd5b60144ea2954c9b490ce2c
prakash126ml <prakash126aws@gmail.com>
2024-01-05 00:02:19 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This is the 1st project template created with the help of cookiecutter',
+    author='prakash',
+    license='',
+)
